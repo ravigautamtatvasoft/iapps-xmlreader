20a5998ed6d770c0358209a55c159b784bc1a96f
ravigautamtatvasoft <ravi.gautam@tatvasoft.com>
2024-04-30 16:45:27 +0530

    IXR-1 -Initia project setup
    (cherry picked from commit b3f68868fbb2d6e58aed5f121490790ddf992566)

---
+++ src/main/java/com/iapps/iappsxmlreader/IappsXmlreaderApplication.java
@@ -0,0 +1,13 @@
+package com.iapps.iappsxmlreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IappsXmlreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IappsXmlreaderApplication.class, args);
+	}
+
+}
